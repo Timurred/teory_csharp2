ac3ed0cc99779c8da7d8a3774909b9d45e368d2d
Yumaguen Timur <timur_red82@mail.ru>
2024-02-20 19:41:39 +0300

    первая  задача по заполнению массива

---
+++ less2/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr =new int[5];
+arr[0] =5;
+arr[1] =7;
+arr[2] =5;
+arr[3] =4;
+arr[4] =3;
+Console.Write(arr[0]);
+Console.Write(arr[1]);
+Console.Write(arr[2]);
+Console.Write(arr[3]);
+Console.Write(arr[4]);
+
+// int [] arr2 = new int[5]{1,2,3,4,5} ; альтер способ ввода
+
+// int[]arr3 ={1,2,3,4,5};
